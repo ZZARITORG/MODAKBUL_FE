53f7a327d8f3431c926b222f6082d620ad69d258
b1anker-kr <b1anker.kr@gmail.com>
2024-10-25 09:27:23 +0900

    [feat] BackButtonAppBar 구현

--- lib/themes/styles.dart
+++ lib/themes/styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'color_schemes.dart';
 
@@ -15,33 +16,41 @@ class Styles {
     appBarTheme: kAppAppBarTheme,
     bottomSheetTheme: kBottomSheetThemeData,
     elevatedButtonTheme: kElevatedButtonThemeData,
+    iconButtonTheme: kIconButtonThemeData,
     useMaterial3: true,
   );
 
   ///Appbar 테마
-  static AppBarTheme kAppAppBarTheme = const AppBarTheme(
+  static AppBarTheme kAppAppBarTheme = AppBarTheme(
     color: Colors.white,
     scrolledUnderElevation: 0,
-    titleSpacing: 16, //여기 디폴트패딩 상수로 넣기
+    titleSpacing: 16.w,
   );
 
   ///BottomSheet 테마
   static BottomSheetThemeData kBottomSheetThemeData =
-      const BottomSheetThemeData(
+  const BottomSheetThemeData(
     backgroundColor: ColorSchemes.white,
   );
 
   ///ElevatedButton 테마
   static ElevatedButtonThemeData kElevatedButtonThemeData =
-      ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-    minimumSize: Size.zero,
-    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-    elevation: 0,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(14),
-    ),
-  ));
+  ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.zero,
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+      ));
+
+  ///IconButton 테마
+  static IconButtonThemeData kIconButtonThemeData =
+  const IconButtonThemeData(
+    style: ButtonStyle(
+      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    ),);
 
 /** static TextTheme textTheme = TextTheme(
     ); */
@@ -51,70 +60,70 @@ class Styles {
 extension CustomStyles on TextTheme {
 
   ///bigHeadLine
-  TextStyle get bigHeadLine1 => const TextStyle(
-      letterSpacing: -1.5,
-      fontSize: 38,
-      fontVariations: <FontVariation>[
+  TextStyle get bigHeadLine1 => TextStyle(
+      letterSpacing: -1.5.sp,
+      fontSize: 38.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 700),]);
-  TextStyle get bigHeadLine2 => const TextStyle(
-      letterSpacing: -1.5,
-      fontSize: 32,
-      fontVariations: <FontVariation>[
+  TextStyle get bigHeadLine2 => TextStyle(
+      letterSpacing: -1.5.sp,
+      fontSize: 32.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 700),]);
-  TextStyle get bigHeadLine3 => const TextStyle(
-      letterSpacing: -0.5,
-      fontSize: 24,
-      fontVariations: <FontVariation>[
+  TextStyle get bigHeadLine3 => TextStyle(
+      letterSpacing: -0.5.sp,
+      fontSize: 24.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 700),]);
-  TextStyle get bigHeadLine4 => const TextStyle(
-      letterSpacing: -0.5,
-      fontSize: 20,
-      fontVariations: <FontVariation>[
+  TextStyle get bigHeadLine4 => TextStyle(
+      letterSpacing: -0.5.sp,
+      fontSize: 20.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 700),]);
-  TextStyle get bigHeadLine5 => const TextStyle(
-      letterSpacing: -0.5,
-      fontSize: 16,
-      fontVariations: <FontVariation>[
+  TextStyle get bigHeadLine5 => TextStyle(
+      letterSpacing: -0.5.sp,
+      fontSize: 16.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 700),]);
 
   ///smallHeadLine
-  TextStyle get smallHeadLine1 => const TextStyle(
-      letterSpacing: 0.25,
-      fontSize: 20,
-      fontVariations: <FontVariation>[
+  TextStyle get smallHeadLine1 => TextStyle(
+      letterSpacing: 0.25.sp,
+      fontSize: 20.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 600),]);
-  TextStyle get smallHeadLine2 => const TextStyle(
-      letterSpacing: 0.25,
-      fontSize: 18,
-      fontVariations: <FontVariation>[
+  TextStyle get smallHeadLine2 => TextStyle(
+      letterSpacing: 0.25.sp,
+      fontSize: 18.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 600),]);
-  TextStyle get smallHeadLine3 => const TextStyle(
-      letterSpacing: 0.25,
-      fontSize: 16,
-      fontVariations: <FontVariation>[
+  TextStyle get smallHeadLine3 => TextStyle(
+      letterSpacing: 0.25.sp,
+      fontSize: 16.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 600),]);
 
   ///body
-  TextStyle get body1 => const TextStyle(
-      letterSpacing: 1.25,
-      fontSize: 18,
-      fontVariations: <FontVariation>[
+  TextStyle get body1 => TextStyle(
+      letterSpacing: 1.25.sp,
+      fontSize: 18.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 500),]);
-  TextStyle get body2 => const TextStyle(
-      letterSpacing: 1.25,
-      fontSize: 16,
-      fontVariations: <FontVariation>[
+  TextStyle get body2 => TextStyle(
+      letterSpacing: 1.25.sp,
+      fontSize: 16.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 500),]);
-  TextStyle get body3 => const TextStyle(
-      letterSpacing: 1.25,
-      fontSize: 14,
-      fontVariations: <FontVariation>[
+  TextStyle get body3 => TextStyle(
+      letterSpacing: 1.25.sp,
+      fontSize: 14.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 500),]);
 
   ///caption
-  TextStyle get caption => const TextStyle(
-      letterSpacing: 1.25,
-      fontSize: 14,
-      fontVariations: <FontVariation>[
+  TextStyle get caption => TextStyle(
+      letterSpacing: 1.25.sp,
+      fontSize: 14.sp,
+      fontVariations: const <FontVariation>[
         FontVariation('wght', 400),]);
 }
