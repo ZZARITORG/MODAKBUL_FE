aefd2e39d9d79a5872d00621ca58382064ee5e94
OEH4 <taesu7442@gmail.com>
2024-10-28 19:04:36 +0900

    [feat] CreateMeetingButton 다트파일 추가

---
+++ lib/widgets/create_meeting_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateMeetingButton extends StatelessWidget {
+  const CreateMeetingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
