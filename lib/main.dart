39e56c03b483a7fef5606ee5b93b38f5adced864
양지원 <yangjiwon@yangjiwon-ui-MacBookPro.local>
2024-10-29 16:52:53 +0900

    [feat] MyModakbulListTile 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:modakbul/routes/routes.dart';
 import 'package:modakbul/themes/styles.dart';
+import 'package:modakbul/widgets/my_modakbul_list_tile.dart';
 
 void main() {
   runApp(
@@ -105,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            MyModakbulListTile(title: 'sd', time: 'sd', profileLength: 5),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
