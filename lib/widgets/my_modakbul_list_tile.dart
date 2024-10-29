3ad96cdf75adec1a92e5625feef840e67b74188e
양지원 <yangjiwon@yangjiwon-ui-MacBookPro.local>
2024-10-29 16:55:15 +0900

    [feat] MyModakBulListTile 구현

--- lib/widgets/my_modakbul_list_tile.dart
+++ lib/widgets/my_modakbul_list_tile.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:modakbul/constants/style_constants.dart';
+import 'package:modakbul/themes/color_schemes.dart';
 import 'package:modakbul/themes/styles.dart';
-
-import '../themes/color_schemes.dart';
 
 class MyModakbulListTile extends StatelessWidget {
   final String title;
