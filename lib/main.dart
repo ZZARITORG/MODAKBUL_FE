71092b7cb6b0d75152c952b26bddc2c17b5eccbc
kimjj0 <bbbrian1028@naver.com>
2024-10-29 17:45:39 +0900

    feat: UserListProfile 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:modakbul/routes/routes.dart';
 import 'package:modakbul/themes/styles.dart';
+import 'package:modakbul/widgets/user_list_profile.dart';
 
 void main() {
   runApp(
@@ -105,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            UserListProfile(userName: 'userName', userId: 'userId'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
