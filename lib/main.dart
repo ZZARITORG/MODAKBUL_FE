370b1f42105750f882b1ad76f4c9d186bb9e1ced
kimjj0 <bbbrian1028@naver.com>
2024-10-29 18:15:16 +0900

    [feat] main.dart 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:modakbul/routes/routes.dart';
 import 'package:modakbul/themes/styles.dart';
-import 'package:modakbul/widgets/user_list_profile.dart';
 
 void main() {
   runApp(
