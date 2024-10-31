8529506982dc57e0ba6b21b2de1c608d404d02b2
b1anker-kr <b1anker.kr@gmail.com>
2024-10-31 20:29:58 +0900

    [feat] BackButtonAppBar 아이콘 추가

--- lib/widgets/back_button_app_bar.dart
+++ lib/widgets/back_button_app_bar.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:modakbul/constants/assets_path.dart';
+import 'package:modakbul/constants/style_constants.dart';
 
-class BackButtonAppBar extends StatelessWidget {
+class BackButtonAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool isActionButton;
   final VoidCallback? onActionPressed;
 
-  const BackButtonAppBar({Key? key, this.isActionButton = false, this.onActionPressed}) : super(key: key);
+  const BackButtonAppBar(
+      {Key? key, this.isActionButton = false, this.onActionPressed})
+      : super(key: key);
 
   factory BackButtonAppBar.actions({required VoidCallback onActionPressed}) =>
       BackButtonAppBar(
@@ -18,18 +22,28 @@ class BackButtonAppBar extends StatelessWidget {
     return AppBar(
       centerTitle: false, ///스타일로 옮기기
       automaticallyImplyLeading: false,
-      leading: IconButton(
-          padding: EdgeInsets.zero,
-          constraints: const BoxConstraints(),
-          onPressed: () => Navigator.pop(context),
-          icon: SvgPicture.asset('아이콘 경로')),
-      actions: [
-        if (isActionButton)
+      leading: Padding(
+        padding: EdgeInsets.only(left: StyleConstants.defaultPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: [
           IconButton(
               padding: EdgeInsets.zero,
               constraints: const BoxConstraints(),
-              onPressed: onActionPressed,
-              icon: SvgPicture.asset('아이콘 경로')),
+              onPressed: () => Navigator.pop(context),
+              icon: SvgPicture.asset(IconPath.arrowBack)),
+        ]),
+      ),
+      actions: [
+        if (isActionButton)
+          Padding(
+            padding: EdgeInsets.only(right: StyleConstants.defaultPadding),
+            child: IconButton(
+                padding: EdgeInsets.zero,
+                constraints: const BoxConstraints(),
+                onPressed: onActionPressed,
+                icon: SvgPicture.asset(IconPath.moreHorizontal)),
+          ),
       ],
     );
   }
@@ -38,4 +52,3 @@ class BackButtonAppBar extends StatelessWidget {
   // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
-
