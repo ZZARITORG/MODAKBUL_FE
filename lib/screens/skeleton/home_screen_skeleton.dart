5a0567d69982cfa20cc5425ef8a15e93fb7ce2b9
kwakmincheol123 <arnold2246@gmail.com>
2024-11-05 18:14:12 +0900

    [feat] Skeleton Loader 추가

---
+++ lib/screens/skeleton/home_screen_skeleton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class SkeletonLoader extends StatelessWidget {
+  final double width;
+  final double height;
+  final BorderRadiusGeometry borderRadius;
+
+  const SkeletonLoader({
+    Key? key,
+    required this.width,
+    required this.height,
+    this.borderRadius = const BorderRadius.all(Radius.circular(14)),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+
+      ),
+    );
+  }
+}
