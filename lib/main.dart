2a42b901556c8dc393a83e95e7609f0ea4dccd2d
kimjj0 <bbbrian1028@naver.com>
2024-11-07 19:49:48 +0900

    [feat] dependency 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:modakbul/routes/routes.dart';
 import 'package:modakbul/themes/styles.dart';
+import 'package:modakbul/widgets/create_meeting_button.dart';
 
 void main() {
   runApp(
@@ -105,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            CreateMeetingButton(type: 'type', content: 'content', onPressed: (){}, icon: 'icon'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
