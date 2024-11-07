bce981f10d501e22eaa7e762236e7852253de3d4
OEH4 <taesu7442@gmail.com>
2024-11-08 00:08:46 +0900

    [feat] userinfocheck 아이콘 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:modakbul/themes/styles.dart';
 import 'package:modakbul/widgets/content_info.dart';
 import 'package:modakbul/widgets/create_meeting_button.dart';
 import 'package:modakbul/widgets/custom_search_bar.dart';
+import 'package:modakbul/widgets/user_info_check.dart';
 
 void main() {
   runApp(
@@ -69,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            ContentInfo(info: '곽민철 외 2명', onPressed: () {}, icon: IconPath.groupOrange200),
+            UserInfoCheck(text: '참여자 정보를 확인하세요.', onPressed: () {}),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
