4cbf7d2910d428c12a81f9d99bcb4daf03a47a91
b1anker-kr <b1anker.kr@gmail.com>
2024-11-16 15:21:41 +0900

    [feat] ContentInfo 수정

--- lib/widgets/content_info.dart
+++ lib/widgets/content_info.dart
@@ -9,13 +9,19 @@ class ContentInfo extends StatelessWidget {
   final String info;
   final VoidCallback? onPressed;
   final String icon;
+  final double? iconHeight;
+  final double? iconWidth;
+  final bool isActivated;
 
-  const ContentInfo({
-    Key? key,
-    required this.info,
-    required this.onPressed,
-    required this.icon,
-  }) : super(key: key);
+  const ContentInfo(
+      {Key? key,
+        required this.info,
+        required this.onPressed,
+        required this.icon,
+        required this.isActivated,
+        this.iconHeight,
+        this.iconWidth})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,24 +30,41 @@ class ContentInfo extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              SizedBox(
-                  width: 24.r,
-                  height: 24.r,
-                  child: SvgPicture.asset(icon, fit: BoxFit.scaleDown)),
-              SizedBox(
-                  width: 2.w
-              ),
-              Text(
-                  info,
-                  style: Theme.of(context)
-                      .textTheme
-                      .body2
-                      .copyWith(color: ColorSchemes.orange100)),
-            ],
+          Expanded(
+            child: Row(
+              children: [
+                SizedBox(
+                    width: 24.r,
+                    height: 24.r,
+                    child: Center(
+                        child: SvgPicture.asset(
+                          icon,
+                          width: iconWidth,
+                          height: iconHeight,
+                        ))),
+                SizedBox(width: 2.w),
+                Flexible(
+                  child: Text(info,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .body2
+                          .copyWith(color: isActivated ? ColorSchemes.orange200 : ColorSchemes.orange100)),
+                ),
+              ],
+            ),
           ),
-          SvgPicture.asset(IconPath.arrowForward15Orange200),
+          SizedBox(width: 6.w,),
+          SizedBox(
+            height: 24.r,
+            width: 24.r,
+            child: Center(
+              child: SvgPicture.asset(
+                isActivated ? IconPath.arrowForward15Orange200 : IconPath.arrowForward15Orange100,
+                width: 8.r,
+              ),
+            ),
+          ),
         ],
       ),
     );
