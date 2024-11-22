01cad769c8f3ff6eda00f213ff446d3428e2988f
kwakmincheol123 <arnold2246@gmail.com>
2024-11-22 12:22:45 +0900

    [feat] FixedModakbulCard 오버플로우 수정

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:modakbul/screens/modakbul/create_modakbul_screen.dart';
 import 'package:modakbul/screens/modakbul/group_select_screen.dart';
 import 'package:modakbul/screens/splash_screen.dart';
 import 'package:modakbul/themes/styles.dart';
+import 'package:modakbul/widgets/fixed_modakbul_card.dart';
 import 'package:modakbul/widgets/home_screen_skeleton.dart';
 import 'package:modakbul/widgets/invited_modakbul_card.dart'; // home_screen_skeleton 파일 import
 
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       routes: Routes.routes,
       theme: Styles.kThemeData,
-      home: const AuthPhoneScreen(),
+      home: const FixedModakbulCard(title: 'ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ', date: '2024-01-12', location: 'ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ'),
     );
   }
 }
