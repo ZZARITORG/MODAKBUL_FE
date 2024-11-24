52db56a108b1ba6bd61f72d00526c34f6d283837
kimjj0 <bbbrian1028@naver.com>
2024-11-24 13:52:27 +0900

    feat: ModakbulDetailCard 위젯 구현

--- lib/screens/modakbul/modakbul_detail_screen.dart
+++ lib/screens/modakbul/modakbul_detail_screen.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modakbul/widgets/back_button_app_bar.dart';
+import 'package:modakbul/widgets/modakbul_detail_card.dart';
 
-class ModakbulDetailScreen extends StatelessWidget {
+class ModakbulDetailScreen extends StatefulWidget {
   const ModakbulDetailScreen({super.key});
 
   @override
+  State<ModakbulDetailScreen> createState() => _ModakbulDetailScreenState();
+}
+
+class _ModakbulDetailScreenState extends State<ModakbulDetailScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      appBar: BackButtonAppBar(),
+      body: Column(
+        children: [
+            ModakbulDetailCard(userName: 'userName', userId: 'userId', posterProfileImage: 'posterProfileImage', profileLength: 4, participantProfileImage: 'participantProfileImage')
+        ],
+      ),
+    );
   }
 }
