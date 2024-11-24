563e6a6bf12ee9455e08dc54c1998737eaf0cc3a
kwakmincheol123 <arnold2246@gmail.com>
2024-11-25 00:21:17 +0900

    [feat] FriendScreenSkeleton 구현

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:modakbul/screens/modakbul/group_select_screen.dart';
 import 'package:modakbul/screens/splash_screen.dart';
 import 'package:modakbul/themes/styles.dart';
 import 'package:modakbul/widgets/fixed_modakbul_card.dart';
+import 'package:modakbul/widgets/friend_screen_skeleton.dart';
 import 'package:modakbul/widgets/home_screen_skeleton.dart';
 import 'package:modakbul/widgets/invited_modakbul_card.dart'; // home_screen_skeleton 파일 import
 
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       routes: Routes.routes,
       theme: Styles.kThemeData,
-      home: const AuthPhoneScreen(),
+      home: const FriendScreenSkeleton(),
     );
   }
 }
