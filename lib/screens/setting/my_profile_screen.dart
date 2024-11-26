36649d3d75cd8a86e44a69273bd1c2474745def9
kwakmincheol123 <arnold2246@gmail.com>
2024-11-26 22:21:16 +0900

    [feat] MyProfileScreen 및 SettingMenu 수정

--- lib/screens/setting/my_profile_screen.dart
+++ lib/screens/setting/my_profile_screen.dart
@@ -27,127 +27,137 @@ class MyProfileScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 33.h),
-            Card(
-              color: Colors.white,
-              elevation: 0,
-              margin: EdgeInsets.zero,
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(StyleConstants.radiusMedium),
-              ),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(14.w, 23.h, 14.w, 25.h),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 33.h),
+                  Card(
+                    color: Colors.white,
+                    elevation: 0,
+                    margin: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(StyleConstants.radiusMedium),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(14.w, 23.h, 14.w, 25.h),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Stack(children: [
-                            Positioned(
-                              child: CircleAvatar(
-                                radius: StyleConstants.circleSizeS,
-                              ),
-                            ),
-                            Positioned(
-                                left: 40.w,
-                                top: 40.h,
-                                child: CircleAvatar(
-                                  backgroundColor: ColorSchemes.orange200,
-                                  radius: StyleConstants.circleSizeXXXXXXXS,
-                                  child: SvgPicture.asset(
-                                      IconPath.photoCameraOrange100,
-                                      width: 13.83.r,
-                                      fit: BoxFit.scaleDown),
-                                )),
-                          ]),
-                          SizedBox(width: 8.w),
-                          Flexible(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                          Expanded(
+                            child: Row(
                               children: [
-                                Text(
-                                  '김지호',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bigHeadLine4
-                                      .copyWith(color: ColorSchemes.gray500),
-                                ),
-                                SizedBox(height: 2.h),
-                                Text(
-                                  'kim_jj0_',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .body3
-                                      .copyWith(color: ColorSchemes.gray400),
-                                ),
+                                Stack(children: [
+                                  Positioned(
+                                    child: CircleAvatar(
+                                      radius: StyleConstants.circleSizeS,
+                                    ),
+                                  ),
+                                  Positioned(
+                                      left: 40.w,
+                                      top: 40.h,
+                                      child: CircleAvatar(
+                                        backgroundColor: ColorSchemes.orange200,
+                                        radius: StyleConstants.circleSizeXXXXXXXS,
+                                        child: SvgPicture.asset(
+                                            IconPath.photoCameraOrange100,
+                                            width: 13.83.r),
+                                      )),
+                                ]),
+                                SizedBox(width: 8.w),
+                                Flexible(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '김지호',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bigHeadLine4
+                                            .copyWith(color: ColorSchemes.gray500),
+                                      ),
+                                      SizedBox(height: 2.h),
+                                      Text(
+                                        'kim_jj0_',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .body3
+                                            .copyWith(color: ColorSchemes.gray400),
+                                      ),
+                                    ],
+                                  ),
+                                )
                               ],
                             ),
+                          ),
+                          SizedBox(width: 32.w),
+                          Text(
+                            '수정하기',
+                            style: Theme.of(context)
+                                .textTheme
+                                .smallHeadLine3
+                                .copyWith(color: ColorSchemes.orange200),
                           )
                         ],
                       ),
                     ),
-                    SizedBox(width: 32.w),
-                    Text(
-                      '수정하기',
-                      style: Theme.of(context)
-                          .textTheme
-                          .smallHeadLine3
-                          .copyWith(color: ColorSchemes.orange200),
-                    )
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 25.h),
+                  Text(
+                    '사용자 설정',
+                    style: Theme.of(context)
+                        .textTheme
+                        .smallHeadLine3
+                        .copyWith(color: ColorSchemes.orange200),
+                  ),
+                  SizedBox(height: 14.h),
+                  SettingMenu.arrow(menu: '계정정보 관리', icon: IconPath.verifiedUser, iconWidth: 16.r, onPressed: () {}),
+                  SizedBox(height: 18.h),
+                  SettingMenu.arrow(menu: '알림설정', icon: IconPath.notificationsBell, iconWidth: 13.r, onPressed: () {}),
+                  SizedBox(height: 18.h),
+                  SettingMenu.arrow(menu: '친구설정', icon: IconPath.friend, iconWidth: 18.r, onPressed: () {}),
+                  SizedBox(height: 32.h),
+                  Text(
+                    '보안',
+                    style: Theme.of(context)
+                        .textTheme
+                        .smallHeadLine3
+                        .copyWith(color: ColorSchemes.orange200),
+                  ),
+                  SizedBox(height: 14.h),
+                  SettingMenu.arrow(menu: '이용약관', iconWidth: 12.r, icon: IconPath.lock),
+                  SizedBox(height: 18.h),
+                  SettingMenu.arrow(menu: '정보', iconWidth: 15.r, icon: IconPath.description),
+                ],
               ),
             ),
-            SizedBox(height: 25.h),
-            Text(
-              '사용자 설정',
-              style: Theme.of(context)
-                  .textTheme
-                  .smallHeadLine3
-                  .copyWith(color: ColorSchemes.orange200),
+
+            Center(
+              child: InkWell(
+                overlayColor: WidgetStateProperty.all(ColorSchemes.orange000),
+                onTap: (){},
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                    children: [
+                  SizedBox(
+                      width: 24.r,
+                      height: 24.r,
+                      child: SvgPicture.asset(IconPath.powerSettingsNew,
+                          width: 18.r)),
+                  SizedBox(width: 2.w),
+                  Text(
+                    '로그아웃',
+                    style: Theme.of(context)
+                        .textTheme
+                        .smallHeadLine3
+                        .copyWith(color: ColorSchemes.orange100),
+                  )
+                ]),
+              ),
             ),
-            SizedBox(height: 14.h),
-            SettingMenu.arrow(menu: '계정정보 관리', icon: IconPath.verifiedUser),
-            SizedBox(height: 18.h),
-            SettingMenu.arrow(menu: '알림설정', icon: IconPath.notificationsBell),
-            SizedBox(height: 18.h),
-            SettingMenu.arrow(menu: '친구설정', icon: IconPath.friend),
-            SizedBox(height: 32.h),
-            Text(
-              '보안',
-              style: Theme.of(context)
-                  .textTheme
-                  .smallHeadLine3
-                  .copyWith(color: ColorSchemes.orange200),
-            ),
-            SizedBox(height: 14.h),
-            SettingMenu.arrow(menu: '이용약관', icon: IconPath.lock),
-            SizedBox(height: 18.h),
-            SettingMenu.arrow(menu: '정보', icon: IconPath.description),
-            SizedBox(height: 50.h),
-            Align(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                  children: [
-                SizedBox(
-                    width: 24.r,
-                    height: 24.r,
-                    child: SvgPicture.asset(IconPath.powerSettingsNew,
-                        width: 18.r, fit: BoxFit.scaleDown)),
-                SizedBox(width: 2.w),
-                Text(
-                  '로그아웃',
-                  style: Theme.of(context)
-                      .textTheme
-                      .smallHeadLine3
-                      .copyWith(color: ColorSchemes.orange100),
-                )
-              ]),
-            )
+            SizedBox(height: 116.h)
           ],
         ),
       ),
--- lib/widgets/setting_menu.dart
+++ lib/widgets/setting_menu.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,29 +12,37 @@ class SettingMenu extends StatelessWidget {
   final VoidCallback? onPressed;
   final String icon;
   final Widget? trailingWidget;
+  final double? iconWidth;
+  final double? iconHeight;
 
   const SettingMenu(
       {Key? key,
       required this.menu,
       required this.onPressed,
       required this.icon,
+      this.iconWidth,
+      this.iconHeight,
       this.trailingWidget})
       : super(key: key);
 
   factory SettingMenu.arrow({
     required String menu,
     required String icon,
+    required double iconWidth,
+    double? iconHeight,
     VoidCallback? onPressed,
   }) =>
       SettingMenu(
         menu: menu,
         icon: icon,
         onPressed: onPressed,
+        iconWidth: iconWidth,
+        iconHeight: iconHeight,
         trailingWidget: Padding(
           padding: EdgeInsets.only(right: 2.w),
           child: SvgPicture.asset(
             IconPath.arrowForward15Gray200,
-            fit: BoxFit.scaleDown,
+            width: 8.r,
           ),
         ),
       );
@@ -40,6 +50,8 @@ class SettingMenu extends StatelessWidget {
   factory SettingMenu.toggle({
     required String menu,
     required String icon,
+    required double iconWidth,
+    double? iconHeight,
     required bool isToggled,
     required ValueChanged<bool> onToggleChanged,
     Color? activeColor, // 스위치 On 상태 색상
@@ -47,6 +59,8 @@ class SettingMenu extends StatelessWidget {
       SettingMenu(
         menu: menu,
         icon: icon,
+        iconWidth: iconWidth,
+        iconHeight: iconHeight,
         trailingWidget: Padding(
           padding: EdgeInsets.only(right: 4.w),
           child: Switch(
@@ -61,21 +75,24 @@ class SettingMenu extends StatelessWidget {
   factory SettingMenu.version({
     required String menu,
     required String icon,
+    required double iconWidth,
+    double? iconHeight,
     VoidCallback? onPressed,
   }) =>
       SettingMenu(
         menu: menu,
         icon: icon,
+        iconWidth: iconWidth,
+        iconHeight: iconHeight,
         onPressed: onPressed,
         trailingWidget:
-            Padding(
-                padding: EdgeInsets.only(right: 2.w),
-                child: Text('0.1.0')),
+            Padding(padding: EdgeInsets.only(right: 2.w), child: Text('0.1.0')),
       );
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return InkWell(
+      overlayColor: WidgetStateProperty.all(ColorSchemes.orange000),
       onTap: onPressed,
       child: Container(
         width: double.infinity,
@@ -83,7 +100,7 @@ class SettingMenu extends StatelessWidget {
             border: Border(
           bottom: BorderSide(color: ColorSchemes.gray100, width: 1.w),
         )),
-        child: Container(
+        child: Padding(
           padding: EdgeInsets.symmetric(vertical: 10.h),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -93,7 +110,9 @@ class SettingMenu extends StatelessWidget {
                   SizedBox(
                       width: 24.r,
                       height: 24.r,
-                      child: SvgPicture.asset(icon, fit: BoxFit.scaleDown)),
+                      child: Center(
+                          child: SvgPicture.asset(icon,
+                              width: iconWidth, height: iconHeight))),
                   SizedBox(width: 2.w),
                   Text(menu,
                       style: Theme.of(context)
