7ed2d633a8eb985529654685e25b79137f539016
kwakmincheol123 <arnold2246@gmail.com>
2024-11-27 02:33:01 +0900

    [feat] FriendSettingScreen 구현

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:modakbul/screens/home/home_screen.dart';
 import 'package:modakbul/screens/setting/alert_setting_screen.dart';
 import 'package:modakbul/screens/setting/common_setting_screen.dart';
 import 'package:modakbul/screens/setting/edit_my_profile_screen.dart';
+import 'package:modakbul/screens/setting/friend_setting_screen.dart';
 import 'package:modakbul/screens/setting/my_profile_screen.dart';
 import 'package:modakbul/widgets/custom_calender_picker.dart';
 import 'package:modakbul/screens/modakbul/create_content_screen.dart';
@@ -46,7 +47,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       routes: Routes.routes,
       theme: Styles.kThemeData,
-      home: const AlertSettingScreen(),
+      home: const FriendSettingScreen(),
     );
   }
 }
