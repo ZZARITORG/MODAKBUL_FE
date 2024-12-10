bfc5eeecaf4fd89e7a368fcc926730d20cb7c826
kwakmincheol123 <arnold2246@gmail.com>
2024-12-10 16:29:01 +0900

    [feat] 로그아웃 기능 구현

--- lib/widgets/log_out_dialog.dart
+++ lib/widgets/log_out_dialog.dart
@@ -2,9 +2,11 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:modakbul/constants/style_constants.dart';
-import 'package:modakbul/themes/color_schemes.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modakbul/routes/routes.dart';
 import 'package:modakbul/themes/styles.dart';
+import 'package:modakbul/themes/color_schemes.dart';
 
 class LogOutDialog extends StatelessWidget {
   const LogOutDialog({super.key});
@@ -14,45 +16,55 @@ class LogOutDialog extends StatelessWidget {
     return BackdropFilter(
       filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
       child: AlertDialog(
-        contentPadding: EdgeInsets.zero,
-        actionsPadding: EdgeInsets.zero,
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 81.5.w),
-              child: Column(
-                children: [
-                  SizedBox(height: 27.h),
-                  Text('로그아웃 되었습니다.',
-                      style: Theme.of(context)
-                          .textTheme
-                          .smallHeadLine2
-                          .copyWith(color: ColorSchemes.orange200)),
-                  SizedBox(height: 14.h),
-                  Text('다시 로그인 해주세요.',
-                      style: Theme.of(context)
-                          .textTheme
-                          .body3
-                          .copyWith(color: ColorSchemes.gray400)),
-                  SizedBox(height: 22.h)
-                ],
+          contentPadding: EdgeInsets.zero,
+          actionsPadding: EdgeInsets.zero,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 81.5.w),
+                child: Column(
+                  children: [
+                    SizedBox(height: 27.h),
+                    Text('로그아웃 되었습니다.',
+                        style: Theme.of(context)
+                            .textTheme
+                            .smallHeadLine2
+                            .copyWith(color: ColorSchemes.orange200)),
+                    SizedBox(height: 14.h),
+                    Text('다시 로그인 해주세요.',
+                        style: Theme.of(context)
+                            .textTheme
+                            .body3
+                            .copyWith(color: ColorSchemes.gray400)),
+                    SizedBox(height: 22.h)
+                  ],
+                ),
               ),
-            ),
-            Divider(
-              thickness: 1,
-              height: 1.h,
-              color: Color(0xFFF1F1F1)
-            ),
-          ],
-        ),
+              Divider(
+                  thickness: 1,
+                  height: 1.h,
+                  color: Color(0xFFF1F1F1)
+              ),
+            ],
+          ),
           actions: [
             InkWell(
-              onTap: () {},
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+
+                final storage = FlutterSecureStorage();
+                await storage.deleteAll();
+
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    Routes.splashScreen,
+                        (route) => false
+                );
+              },
               overlayColor: WidgetStateProperty.all(Colors.transparent),
               child: Container(
-                width: double.infinity, // InkWell의 영역을 Dialog 너비까지 확장
-                padding: EdgeInsets.symmetric(vertical: 14.5.h), // 위아래 여백 설정
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 14.5.h),
                 alignment: Alignment.center,
                 child: Text(
                   '확인',
