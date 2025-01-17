76d1e4183e50d76136ccc051eada9eaddb686e14
kwakmincheol123 <arnold2246@gmail.com>
2025-01-17 13:02:21 +0900

    [feat] IOS 토큰 코드 수정

--- lib/screens/auth/auth_code_screen.dart
+++ lib/screens/auth/auth_code_screen.dart
@@ -131,9 +131,9 @@ class _AuthCodeScreenState extends State<AuthCodeScreen> {
       // 개발자 권한 받으면 변경 예정
       String? fcmToken;
       if (Platform.isIOS) {
-        fcmToken = dotenv.env['FCM_TOKEN'] ?? '';
+        //fcmToken = dotenv.env['FCM_TOKEN'] ?? '';
         // await Future.delayed(Duration(seconds: 2));
-        // fcmToken = await FirebaseMessaging.instance.getToken();
+        fcmToken = await FirebaseMessaging.instance.getAPNSToken();
         print('APNS Token: $fcmToken');
       } else if (Platform.isAndroid) {
         fcmToken = await FirebaseMessaging.instance.getToken();
