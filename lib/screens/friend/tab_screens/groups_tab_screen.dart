afc84642db07f84a1fa7bf29d421eeeaa7862b89
kimjj0 <bbbrian1028@naver.com>
2025-01-21 23:20:17 +0900

    feat: 프로필 클릭 이벤트 추가

--- lib/screens/friend/tab_screens/groups_tab_screen.dart
+++ lib/screens/friend/tab_screens/groups_tab_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logger/logger.dart';
-import 'package:lottie/lottie.dart';
 import 'package:modakbul/constants/assets_path.dart';
 import 'package:modakbul/constants/style_constants.dart';
 import 'package:modakbul/models/group_list.dart';
