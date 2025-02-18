af53667ec4c7bcb0b5dd6dcf9b5e1e7de281c970
b1anker-kr <b1anker.kr@gmail.com>
2025-02-18 15:13:54 +0900

    [feat] 시간 유틸 수정

--- lib/utils/date_time_utils.dart
+++ lib/utils/date_time_utils.dart
@@ -16,11 +16,12 @@ class DateTimeUtils {
       // UTC 시간 가져오기
       DateTime utcTime = await NTP.now();
       // UTC -> KST 변환
-      return utcTime.add(const Duration(hours: 9));
+      return utcTime;
+      //return utcTime.add(const Duration(hours: 9));
     } catch (e) {
       ///로컬 시간 반환 대신 다른 방법 생각해봐야함
       // NTP 실패 시, 로컬 시간 반환 (예외 처리)
-      return DateTime.now().toUtc().add(const Duration(hours: 9));
+      return DateTime.now().toUtc();
     }
   }
 }
